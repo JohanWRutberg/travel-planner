9849b3d421c5b7a39bbfb4c6fbef2c437e8e7690
JohanWRutberg <johan.w.rutberg@gmail.com>
2025-08-14 21:51:49 +0200

    Implement Google authentication and update Navbar with Google icon

--- lib/auth-actions.ts
+++ lib/auth-actions.ts
@@ -1,8 +1,9 @@
+"use server";
+
 import { signIn, signOut } from "../auth";
-import { redirect } from "next/dist/server/api-utils";
 
 export const login = async () => {
-  await signIn("github", { redirectTo: "/" });
+  await signIn("google", { redirectTo: "/" });
 };
 
 export const logout = async () => {
