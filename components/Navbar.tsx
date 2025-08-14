bfe328ca0c1ea9e37ce473ff5004fa603e17d30e
JohanWRutberg <johan.w.rutberg@gmail.com>
2025-08-14 21:06:32 +0200

    Implement authentication with GitHub and update Navbar for login functionality

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { login } from "@/lib/auth-actions";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -19,7 +22,10 @@ export default function Navbar() {
           <Link href={"/globe"} className="text-slate-900 hover:text-sky-500">
             Globe
           </Link>
-          <button className="flex items-center justify-center bg-gray-800 hover:bg-gray-900 text-white p-2 rounded-sm cursor-pointer">
+          <button
+            className="flex items-center justify-center bg-gray-800 hover:bg-gray-900 text-white p-2 rounded-sm cursor-pointer"
+            onClick={login}
+          >
             Sign In
             <svg
               className="w-6 h-6 ml-2"
---
+++ lib/auth-actions.ts
@@ -0,0 +1,10 @@
+import { signIn, signOut } from "../auth";
+import { redirect } from "next/dist/server/api-utils";
+
+export const login = async () => {
+  await signIn("github", { redirectTo: "/" });
+};
+
+export const logout = async () => {
+  await signOut({ redirectTo: "/" });
+};
